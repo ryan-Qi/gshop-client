d657e6b1d63223377bf63603dca6704b2a157489
ryanQi <ryan-1@163.com>
2020-02-05 21:06:22 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+/*
+入口js
+ */
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+let vm = new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
+
+Vue.use({
+  vm
+})
